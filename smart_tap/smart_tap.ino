9763e441abe77414264dafe9ee1d3923a7e35ba6
n1shanpradhan <105001135+Nishan-Pradhan06@users.noreply.github.com>
2023-01-15 14:15:40 +0545

    Add files via upload

---
+++ smart_tap/smart_tap.ino
@@ -0,0 +1,73 @@
+//Ultasonic Sensor
+#include <Servo.h>
+//Pins connected to the ultrasonic sensor
+const int trigPin = 8;
+const int echoPin = 7;
+const int led = 6;
+Servo myservo;
+
+int range = 10;  //range in inches
+
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  //initialize the sensor pins
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  myservo.attach(9);
+}
+void loop() {
+  // establish variables for duration of the ping,
+  // and the distance result in inches and centimeters:
+  long duration, inches, cm;
+
+  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, LOW);
+
+  // Take reading on echo pin
+  duration = pulseIn(echoPin, HIGH);
+
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  if (inches < 10) {
+    Serial.println("hand puted");
+    digitalWrite(led, HIGH);
+    myservo.write(90);
+    delay(100);
+
+  } else {
+    Serial.println("no hand");
+    digitalWrite(led, LOW);
+    myservo.write(0);
+    delay(100);
+  }
+
+  delay(200);
+}
+
+long microsecondsToInches(long microseconds) {
+
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
