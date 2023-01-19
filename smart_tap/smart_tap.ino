7a2d93f315469b01efd132c0bdb049e9f970d6fa
NISHAN PRADHAN <105001135+Nishan-Pradhan06@users.noreply.github.com>
2023-01-19 14:13:19 +0545

    Update smart_tap.ino

--- smart_tap/smart_tap.ino
+++ smart_tap/smart_tap.ino
@@ -68,7 +68,7 @@ long microsecondsToInches(long microseconds) {
 }
 
 long microsecondsToCentimeters(long microseconds) {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+
   // The ping travels out and back, so to find the distance of the
   // object we take half of the distance travelled.
   return microseconds / 29 / 2;
